f6547f0be5056549524a5aa7234b89a5454f3891
NthngAlone <nthngalone@gmail.com>
2024-06-08 21:24:00 -0400

    upates

--- mhlc-ui/src/layouts/default/AppBar.vue
+++ mhlc-ui/src/layouts/default/AppBar.vue
@@ -1,62 +1,74 @@
 <template>
-  <v-app-bar>
-    <v-app-bar-title>
-
-    </v-app-bar-title>
-
-    <v-spacer></v-spacer>
-
-    {{ churchInfo.streetAddress }} | {{ churchInfo.city }}, {{ churchInfo.state}} {{ churchInfo.zipCode }} | {{ churchInfo.phoneNumber }}
-
-    <v-btn icon>
-      <v-icon>mdi-facebook</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>mdi-instagram</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>mdi-youtube</v-icon>
-    </v-btn>
-
-    <template v-slot:extension>
-      {{ churchInfo.slogan }}
-      <v-spacer />
-      <v-menu
-        v-for="(item) in menuItems"
-        v-bind:key="item.id"
-        open-on-hover
-      >
-        <template v-slot:activator="{ props }">
-          <v-btn
-            color="primary"
-            v-bind="props"
-            :to="item.path"
-            text
-          >
-            {{ item.label }}
-          </v-btn>
-        </template>
-
-        <v-list v-if="item.children.length > 0">
-          <v-list-item
-            v-for="(child) in item.children"
-            :key="child.id"
-          >
-            <v-list-item-title>
-              <v-btn
-                :to="child.path"
-                plain
-              >
-                {{ child.label }}
+  <v-container class="header">
+    <v-row>
+      <v-col cols="4">
+        <img src="/logo193x185.png" />
+        <br />
+        {{ churchInfo.slogan }}
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col cols="8">
+        <v-container>
+          <v-row>
+            <v-col class="social">
+              <v-btn icon>
+                <v-icon>mdi-facebook</v-icon>
               </v-btn>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </template>
-  </v-app-bar>
+
+              <v-btn icon>
+                <v-icon>mdi-instagram</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-youtube</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="contact">
+              {{ churchInfo.streetAddress }}<br />{{ churchInfo.city }}, {{ churchInfo.state}} {{ churchInfo.zipCode }}<br />{{ churchInfo.phoneNumber }}
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="menu">
+              <v-menu
+                v-for="(item) in menuItems"
+                v-bind:key="item.id"
+                open-on-hover
+              >
+                <template v-slot:activator="{ props }">
+                  <v-btn
+                    color="primary"
+                    v-bind="props"
+                    :to="item.path"
+                    variant="text"
+                  >
+                    {{ item.label }}
+                  </v-btn>
+                </template>
+
+                <v-list v-if="item.children.length > 0">
+                  <v-list-item
+                    v-for="(child) in item.children"
+                    :key="child.id"
+                  >
+                    <v-list-item-title>
+                      <v-btn
+                        :to="child.path"
+                        plain
+                      >
+                        {{ child.label }}
+                      </v-btn>
+                    </v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
@@ -73,5 +85,9 @@
 .v-list-item .v-btn--variant-elevated, .v-list-item .v-btn--elevated:hover, .v-list-item .v-btn--elevated:focus {
     box-shadow: none;
 }
-header.v-toolbar.v-app-bar { background-image: url("/logo193x185.png"); background-repeat: no-repeat; height: 225px; }
+/* header.v-toolbar.v-app-bar { background-image: url("/logo193x185.png"); background-repeat: no-repeat; height: 225px; } */
+.v-col.menu { text-align: right; }
+.v-col.contact { text-align: right; }
+.v-col.social { text-align: right; }
+.v-container.header { padding-top: 4px; padding-bottom: 4px; }
 </style>
