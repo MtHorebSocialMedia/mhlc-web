95ac3942b39b54990566aed44581be12b8272fbb
NthngAlone <nthngalone@gmail.com>
2024-07-11 13:13:28 -0400

    more responsive fixes with header

---
+++ docs/assets/ssrBoot-54d9cb98.js
@@ -0,0 +1,1 @@
+import{s,o as t,c as e,an as n}from"./index-647c5bd8.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
--- mhlc-ui/src/layouts/default/AppBar.vue
+++ mhlc-ui/src/layouts/default/AppBar.vue
@@ -2,7 +2,11 @@
   <v-container class="header">
     <v-row>
       <v-col cols="4">
-        <img src="/logo193x185.png" />
+        <img
+          :width="xs ? 135 : 193"
+          :height="xs ? 130 : 185"
+          src="/logo193x185.png"
+        />
         <br />
         {{ churchInfo.slogan }}
       </v-col>
@@ -30,11 +34,11 @@
                   {{ churchInfo.phoneNumber }}
               </span>
               <span v-else>
-                <v-btn icon>
-                  <v-icon>mdi-map-marker</v-icon>
+                <v-btn icon @click="showAddress = true">
+                  <v-icon color="blue">mdi-map-marker</v-icon>
                 </v-btn>
-                <v-btn icon>
-                  <v-icon>mdi-phone</v-icon>
+                <v-btn icon @click="showPhone = true">
+                  <v-icon color="green">mdi-phone</v-icon>
                 </v-btn>
               </span>
             </v-col>
@@ -48,17 +52,73 @@
       </v-col>
     </v-row>
   </v-container>
+  <v-dialog
+    v-model="showAddress"
+    width="auto"
+  >
+      <v-card width="400">
+          <v-card-title>
+              <v-icon color="blue">mdi-map-marker</v-icon>
+              Mt. Horeb Lutheran Church
+              <hr />
+          </v-card-title>
+          <v-container>
+            <v-row>
+              <v-col>
+                {{ churchInfo.streetAddress }}<br />
+                {{ churchInfo.city }}, {{ churchInfo.state}} {{ churchInfo.zipCode }}
+              </v-col>
+            </v-row>
+          </v-container>
+          <template v-slot:actions>
+              <v-btn
+                  class="ms-auto"
+                  text="Close"
+                  @click="showAddress = false"
+              ></v-btn>
+          </template>
+      </v-card>
+  </v-dialog>
+  <v-dialog
+    v-model="showPhone"
+    width="auto"
+  >
+      <v-card width="400">
+          <v-card-title>
+              <v-icon color="green">mdi-phone</v-icon>
+              Mt. Horeb Lutheran Church
+              <hr />
+          </v-card-title>
+          <v-container>
+            <v-row>
+              <v-col>
+                {{ churchInfo.phoneNumber }}
+              </v-col>
+            </v-row>
+          </v-container>
+          <template v-slot:actions>
+              <v-btn
+                  class="ms-auto"
+                  text="Close"
+                  @click="showPhone = false"
+              ></v-btn>
+          </template>
+      </v-card>
+  </v-dialog>
 </template>
 
 <script setup>
-    import Menu from './Menu.vue'
+    import Menu from '../../components/Menu.vue'
     import { useContentStore } from '@/store/content';
     import { storeToRefs } from 'pinia';
     import { useDisplay } from 'vuetify'
+    import { ref } from 'vue';
 
     const contentStore = useContentStore();
     const { churchInfo, menuItems } = storeToRefs(contentStore);
-    const { lgAndUp, smAndUp } = useDisplay()
+    const { lgAndUp, smAndUp, xs } = useDisplay()
+    const showAddress = ref(false);
+    const showPhone = ref(false);
 
 </script>
 <style>
@@ -72,4 +132,5 @@
 .v-col.contact { text-align: right; }
 .v-col.social { text-align: right; }
 .v-container.header { padding-top: 4px; padding-bottom: 4px; }
+.header .v-btn--icon { margin-left: 5px; }
 </style>
