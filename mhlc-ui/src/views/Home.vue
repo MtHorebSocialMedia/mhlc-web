9c9e6199ce6ffc01cf882d81078dbd5c7837d94f
NthngAlone <nthngalone@gmail.com>
2024-07-13 13:43:31 -0400

    made video on home page responsive

--- mhlc-ui/src/views/Home.vue
+++ mhlc-ui/src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <v-container class="home">
     <h2>Welcome to Mt. Horeb Lutheran Church!</h2>
+    <hr />
     <v-alert style="text-align: center">
-      <p>Mt. Horeb Lutheran Church is located in Chapin, SC and is a member of the Evangelical Lutheran Church of America (ELCA).</p>
-      <p>We are a vibrant congregation steeped in a rich history and looking torward a promising future!</p>
+      <p>Mt. Horeb Lutheran Church is a community of faith located in Chapin, SC and is a part of the Evangelical Lutheran Church of America (ELCA).</p>
+      <p>We are a vibrant congregation steeped in a rich history while also looking forward torwards a promising future!</p>
       <p>
         <strong>Come worship with us on Sunday mornings at 10am!</strong>
       </p>
     </v-alert>
     <v-container class="d-flex justify-center">
       <iframe
-        width="560"
-        height="315"
+        :width="videoWidth"
+        :height="videoHeight"
         src="https://www.youtube.com/embed/FywyUyCav4A?si=OL6EtBX38sBYP2h7"
         title="YouTube video player"
         frameborder="0"
@@ -24,9 +25,33 @@
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import { useDisplay } from 'vuetify'
+  import { computed } from 'vue';
+  const { xs, sm } = useDisplay();
+  const videoWidth = computed(() => {
+    const originalSize = 560;
+    if (xs.value) {
+      return originalSize * .7;
+    } else if (sm.value) {
+      return originalSize * .85;
+    } else {
+      return originalSize;
+    }
+  })
+  const videoHeight = computed(() => {
+    const originalSize = 315;
+    if (xs.value) {
+      return originalSize * .7;
+    } else if (sm.value) {
+      return originalSize * .85;
+    } else {
+      return originalSize;
+    }
+  })
 </script>
 
 <style scoped>
+  .v-container.home h2 { margin-top: 10px; margin-bottom: 10px; }
+  .v-container.home hr { margin-bottom: 20px; }
   .home p { margin-bottom: 10px; }
 </style>
