0308be04836fbe9c59e4c68d7b52308614af8599
NthngAlone <nthngalone@gmail.com>
2024-08-30 09:35:03 -0400

    fixed video ids

--- mhlc-content-api/src/youtubeService.js
+++ mhlc-content-api/src/youtubeService.js
@@ -4,8 +4,22 @@ const rssParser = new RssParser();
 async function getVideosList() {
 
     const feed = await rssParser.parseURL(process.env.YOUTUBE_RSS_FEED);
-    // TODO filter out non live streams?
-    return feed.items;
+
+    return feed.items
+        .filter(() => {
+            // TODO filter out non live streams
+            return true;
+        })
+        .map((video) => {
+            const [,,id] = video.id.split(':');
+            return {
+                id,
+                title: video.title,
+                link: video.link,
+                date: video.pubDate,
+                author: video.author
+            }
+        });
 }
 
 module.exports = {
