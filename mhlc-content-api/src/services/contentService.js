28fc0e9d551069fa57cf84bf77b897e9c1428cec
David Williams <nthngalone@gmail.com>
2026-03-09 21:53:28 -0400

    fixing removing of stale entry caches

--- mhlc-content-api/src/services/contentService.js
+++ mhlc-content-api/src/services/contentService.js
@@ -59,7 +59,7 @@ function writeContentfulAuditEvent(resource) {
 
 async function clearCachedContent(contentId) {
     if (contentId) {
-        await removeCacheEntry(contentId.id);
+        await removeCacheEntry(`entry-${contentId.id}`);
         await removeCacheEntry(contentId.contentType, true);
     } else {
         await clearCache();
